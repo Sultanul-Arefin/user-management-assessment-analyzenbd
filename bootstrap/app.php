4cf5e3e80dcaaa3c7b1939a6c6e3e14d4e851f9c
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-03-16 21:24:48 +0600

    custom exception added for constant API response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Middleware\ForceJsonResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +24,69 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // METHOD NOT ALLOWED CUSTOM EXCEPTION
+        $exceptions->render(function(MethodNotAllowedHttpException $e, Request $request){
+            if($request->is('api/*')){
+                $response = [
+                    'data' => [],
+                    'status' => 'error',
+                    'message' => 'Method Not Allowed!',
+                ];
+                return response()->json($response, 405);
+            }
+        });
+        // VALIDATION CUSTOM EXCEPTION
+        $exceptions->render(function(ValidationException $e, Request $request){
+            if($request->is('api/*')){
+                $response = [
+                    'status' => 'error',
+                    'message' => implode(
+                        ',',
+                        collect($e->errors())
+                            ->flatten()
+                            ->toArray()
+                    ),
+                    'data' => [
+                        'errors' => $e->errors(),
+                    ],
+                ];
+                return response()->json($response, 422);
+            }
+        });
+        // HTTP NOT FOUND CUSTOM EXCEPTION
+        $exceptions->render(function(NotFoundHttpException $e, Request $request){
+            if($request->is('api/*')){
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Url Not Found!',
+                    'data' => [],
+                ];
+    
+                return response()->json($response, 404);
+            }
+        });
+        // HTTP CUSTOM EXCEPTION
+        $exceptions->render(function(HttpException $e, Request $request){
+            if($request->is('api/*')){
+                $response = [
+                    'status' => 'error',
+                    'message' => $e->getMessage(),
+                    'data' => [],
+                ];
+    
+                return response()->json($response, $e->getStatusCode());
+            }
+        });
+        // MODEL/RESOURCE NOT FOUND CUSTOM EXCEPTION
+        $exceptions->render(function(ModelNotFoundException $e, Request $request){
+            if($request->is('api/*')){
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Resource Not Found',
+                    'data' => [],
+                ];
+    
+                return response()->json($response, 404);
+            }
+        });
     })->create();
