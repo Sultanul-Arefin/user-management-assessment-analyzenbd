1930191dce01d609d860e1692cc02b22cf32c6f5
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-03-19 03:39:18 +0600

    address factory updated

---
+++ database/factories/UserAddressFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\UserAddress;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserAddress>
+ */
+class UserAddressFactory extends Factory
+{
+    protected $model = UserAddress::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'address' => fake()->address,
+        ];
+    }
+}
