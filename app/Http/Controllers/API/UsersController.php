b598966324b60948a79168100825b39564da8952
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-03-19 02:12:38 +0600

    updated the event name

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Events\UserCreatedEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
@@ -15,7 +16,7 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     use UserAddress;
-    
+
     public function __construct(
         public UserRepositoryInterface $userRepo
     )
@@ -49,6 +50,7 @@ class UsersController extends Controller
                     'created_by'
                 ])
             );
+            event(new UserCreatedEvent($user, $request->address));
         } catch(Exception $e){
             throw new CustomException('Oops! something wrong, please try again', 422);
         }
